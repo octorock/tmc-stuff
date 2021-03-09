603595e2ab82b1ee760efe9ade056b02d8da71cd
octorock <79596758+octorock@users.noreply.github.com>
2021-03-09 09:15:14 +0100

    Add colors to build

--- README.md
+++ README.md
@@ -8,4 +8,7 @@ The python scripts used to first extract the scripts from the rom and create asm
 [Convert u8 array to u32 array](u8tou32array.py)
 
 ### Find out where scripts belong to
-[sort_scripts.sh](sort_scripts.sh)
+Finds the files that the functions are defined in that are referenced by `Call` commands in scripts: [sort_scripts.sh](sort_scripts.sh)
+
+### Colorize Output of Build
+Colors stderr orange or red, depending on if a line contains `error`. Prints stdout in gray, except for final tmc: OK message.[make.sh](make.sh)
---
+++ make.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Color output
+# https://serverfault.com/a/59279
+
+# ignore case while string contains
+# https://unix.stackexchange.com/a/132485
+
+TMC_FOLDER="../github"
+
+cd $TMC_FOLDER
+shopt -s nocasematch
+
+make -j$(nproc) 2> >(while read line; do 
+# stderr
+if [[ $line =~ "error" ]]; then
+    echo -e "\e[01;31m$line\e[0m" >&2;
+else
+    echo -e "\e[33m$line\e[0m" >&2;
+fi
+done) 1> >(while read line; do 
+# stdout
+if [[ $line == "tmc.gba: FAILED" ]]; then
+    echo -e "tmc.gba: \e[01;31mFAILED\e[0m" >&1;
+elif [[ $line == "tmc.gba: OK" ]]; then
+    echo -e "tmc.gba: \e[01;32mOK\e[0m" >&1;
+else
+    echo -e "\e[90m$line\e[0;0m" >&1;
+fi
+done)
