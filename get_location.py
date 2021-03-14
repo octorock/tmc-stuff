80e9847cca957ca120b889a67f37cc8065947193
octorock <79596758+octorock@users.noreply.github.com>
2021-03-15 00:00:10 +0100

    Directly copy from and to the clipboard for a faster workflow

--- get_location.py
+++ get_location.py
@@ -1,52 +1,71 @@
 import sys
 from colors import Color
+from search_symbol import parse_rom_structure, get_structure
+import pyperclip
 
 STRUCT_FILE = 'tmp/structure.txt'
 
 
-structure = []
+# TODO build with symbol granularity as well 
+# structure = []
 
-# parse rom structure
-with open(STRUCT_FILE, 'r') as file:
-    for line in file:
-        arr = line.split(' ')
+# # parse rom structure
+# with open(STRUCT_FILE, 'r') as file:
+#     for line in file:
+#         arr = line.split(' ')
         
-        structure.append({
-            'start': int(arr[0], 16),
-            'file': arr[1],
-            'section': arr[2].strip(),
-            'in_ptrs': [],
-            'out_ptrs': []
-        })
+#         structure.append({
+#             'start': int(arr[0], 16),
+#             'file': arr[1],
+#             'section': arr[2].strip(),
+#             'in_ptrs': [],
+#             'out_ptrs': []
+#         })
 
 
-# append final structure to avoid need to test for end
-structure.append({
-    'start': 0x08ffffff,
-    'file': 'none',
-    'section': 'none',
-    'in_ptrs': [],
-    'out_ptrs': []
-})
+# # append final structure to avoid need to test for end
+# structure.append({
+#     'start': 0x08ffffff,
+#     'file': 'none',
+#     'section': 'none',
+#     'in_ptrs': [],
+#     'out_ptrs': []
+# })
 
-def get_structure(addr): # TODO could certainly be optimized
-    i = 0
-    while addr >= structure[i+1]['start']:
-        i += 1
-    return structure[i]
+# def get_structure(addr): # TODO could certainly be optimized
+#     i = 0
+#     while addr >= structure[i+1]['start']:
+#         i += 1
+#     return structure[i]
 
+
+# Structure in symbol granularity
+structure = parse_rom_structure()
 
 def main():
-    print('Enter hex addr of location:')
+    print('Enter hex addr of location: ', end='')
+    sys.stdout.flush()
     for line in sys.stdin:
+        from_clipboard = False
+        if line == '\n':
+            from_clipboard = True
+            line = pyperclip.paste()
         try:
             addr = int(line, 16)
-            struct = get_structure(addr)
+            struct = get_structure(structure, addr)
             print(f'Located {Color.Green}{hex(addr)}{Color.Off}')
-            print(f'In file: {Color.Blue}{struct["file"]} {Color.IBlack}({struct["section"]}){Color.Off}')
+            print(f'In symbol: {Color.Purple}{struct["symbol"]}{Color.Off}')
+            print(f'In file: {Color.Blue}{struct["file"]} {Color.Off}') # {Color.IBlack}({struct["section"]})
             print(f'With offset: {Color.Yellow}{hex(addr-struct["start"])}{Color.Off}')
+
+            if from_clipboard:
+                # Copy filename without .o extension to be easily able to find the file
+                pyperclip.copy(struct['file'][:-2])
         except:
             print(f'{Color.Red}Not a valid hex number.{Color.Off}', file=sys.stderr)
+        print()
+        print('Enter hex addr of location: ', end='')
+        sys.stdout.flush()
 
 if __name__ == '__main__':
     main()
--- ghidra_ptr_list_to_asm.py
+++ ghidra_ptr_list_to_asm.py
@@ -1,16 +1,38 @@
 import sys
 from colors import Color
+import pyperclip
 
 # Convert a list of pointers in Ghidra by pressing p
 # Then select all of them and copy. Insert into this script and press Ctrl+D
-
-# To directly copy to clipboard run python ghidra_ptr_list_to_asm.py | xclip -selection clipboard
+# The asm code is then copied to the clipboard.
 def main():
-    for line in sys.stdin.readlines():
-        
-        arr = line.split()
-        if len(arr) >= 7:
-            print(f'.4byte {arr[6]}')
+    while True:
+        print(f'{Color.Blue}Copy pointers from Ghidra into clipboard, then press Enter: {Color.Off}')
+
+        input()
+
+        code = []
+        input_lines = pyperclip.paste().split('\n')
+        # Read until Ctrl+D
+        for line in input_lines:
+
+
+            arr = line.split()
+            if len(arr) >= 7:
+                addr = arr[6]
+                if 'sub_' in addr and addr.endswith('+1'): # The +1 for function pointers is not necessary
+                    addr = addr[:-2]
+
+                if not 'Point(*)' in addr:
+                    code.append(f'\t.4byte {addr}')
+
+        if len(code) == 0:
+            print(f'{Color.Red}No Ghidra pointers found in clipboard: {Color.Off}')
+            print('\n'.join(input_lines))
+            continue
+        print('\n'.join(code))
+        pyperclip.copy('\n'.join(code))
+        print(f'{Color.Green}Copied to clipboard.{Color.Off}')
         # print(Color.Green + str(arr) + Color.Off)
 
 if __name__ == '__main__':
